86cade88ec9d48ce423a9a8a90876aa2f57e0a5b
wwewwt <wwewwt@163.com>
2024-01-27 01:09:55 +0800

    Update start.py
    print path

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os
 import torch
 from modelscope import snapshot_download
+os.system('pwd')
 model_dir = snapshot_download('wwewwt/zuchongzhi', cache_dir='./model', revision='v1.0.0')
 os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
